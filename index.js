4581bee6371fff164e98f220d04731fff11e6e80
stebeus <189894010+stebeus@users.noreply.github.com>
2026-05-01 08:45:22 -0300

    feat: add renderGif

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ const searchInput = document.querySelector('.search-input');
 const initialGifPath = '/images/sonic-waiting.gif';
 
 const isEmpty = (query) => query.trim() === '';
+
+function renderGif(title = "I'm waiting for you...", url) {
+	const gifTitle = document.querySelector('h2');
+	const gif = document.querySelector('.gif');
+
+	gifTitle.textContent = title;
+	gif.src = url;
+}
